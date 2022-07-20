b9cf86a30e9914b57d043cb4600400ea8cf2259f
Firose Hussain <github@firumon.com>
2022-07-20 17:23:45 +0530

    Index component color modified

--- install/src/pages/IndexPage.vue
+++ install/src/pages/IndexPage.vue
@@ -2,11 +2,11 @@
   <q-page class="flex flex-center column">
     <img alt="Elements Logo" src="~assets/favicon-128x128.png" style="width: 100px; height: 100px">
     <q-card>
-      <q-card-section class="bg-red text-white text-bold" style="width: 300px">Imported Data Counts</q-card-section>
+      <q-card-section class="bg-secondary text-white text-bold" style="width: 300px">Imported Data Counts</q-card-section>
       <q-list dense separator>
         <q-item v-for="ITEM in master_master" :key="ITEM">
           <q-item-section>{{ ITEM }}</q-item-section>
-          <q-item-section side class="text-info">{{ size(master[ITEM]) }}</q-item-section>
+          <q-item-section side class="text-secondary text-bold">{{ size(master[ITEM]) }}</q-item-section>
         </q-item>
       </q-list>
     </q-card>
