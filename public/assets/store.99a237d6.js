2f856c16b820b6faf9a4f008d636911f590c0dd7
Anju KV <anju@milestoneit.net>
2022-07-22 12:38:53 +0530

    anju is commit

---
+++ public/assets/store.99a237d6.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index.9b73df50.js";import{u as s}from"./master.b37525e5.js";var p=r(async a=>{AssetWorker.onmessage=function(t){const{type:o,payload:e}=t.data;s()[o](e)}});export{p as default};
