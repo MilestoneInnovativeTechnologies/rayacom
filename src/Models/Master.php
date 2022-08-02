e8570053dcb93a4f04be21b765631f12c87e3c5e
Firose Hussain <github@firumon.com>
2022-08-02 11:02:20 +0530

    Master data updating for all prevented

--- src/Models/Master.php
+++ src/Models/Master.php
@@ -2,8 +2,6 @@
 
 namespace Milestone\Rayacom\Models;
 
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
@@ -18,6 +16,7 @@ class Master extends Model
                 Property::set($master->master,$property,$master->id,$master->$property);
                 $master->offsetUnset($property);
             }
+            return $master->isDirty('name');
         });
         static::creating(function($master){
             $properties = array_values(array_diff(array_keys($master->getAttributes()),['id','name','master','created_at','updated_at']));
@@ -34,9 +33,11 @@ class Master extends Model
             }
             self::$RFCAs = [];
         });
-        static::saved(function(){
-            Cache::forever(rayacom_config('cache_key.db_master_data_max_times'),DB::table('_master_data')->select(['master', DB::raw('max(updated_at) as `max`')])->groupBy('master')->pluck('max', 'master')->toArray());
-            Cache::forever(rayacom_config('cache_key.db_master_data'),DB::table('_master_data')->get()->groupBy->master->map(fn($masters) => $masters->map(fn($master) => [intval($master->id), trim($master->name)]))->toArray());
+        static::saved(function($master){
+            if($master->wasChanged('name')){
+                Cache::forever(rayacom_config('cache_key.db_master_data_max_times'),DB::table('_master_data')->select(['master', DB::raw('max(updated_at) as `max`')])->groupBy('master')->pluck('max', 'master')->toArray());
+                Cache::forever(rayacom_config('cache_key.db_master_data'),DB::table('_master_data')->get()->groupBy->master->map(fn($masters) => $masters->map(fn($master) => [intval($master->id), trim($master->name)]))->toArray());
+            }
         });
     }
 
