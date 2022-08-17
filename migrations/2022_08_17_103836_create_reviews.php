f34cc853eaf38d991b23924e363a69419c7d17b1
sona-viswam <sona.viswam@gmail.com>
2022-08-17 11:24:05 +0530

    new tables for leave, review, offes have added

---
+++ migrations/2022_08_17_103836_create_reviews.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+          $table->id();
+          $table->bigInteger('customer')->index();
+          $table->enum('type',['Product','Order','SalesExecutive'])->default('Product');
+          $table->string('comment',512)->nullable();
+          $table->enum('status',['New','Viewed'])->default('New');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
