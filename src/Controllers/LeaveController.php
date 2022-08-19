72f72f51a3a4bf4bfc8927eec66cbe772f4b6c83
Firose Hussain <github@firumon.com>
2022-08-19 16:28:56 +0530

    Corrected

--- src/Controllers/LeaveController.php
+++ src/Controllers/LeaveController.php
@@ -22,8 +22,18 @@ class LeaveController extends Controller
         $latest_date = request()->header('Latest-Date','2000-01-01 00:00:01') ?: '2000-01-01 00:00:01';
         if(Carbon::parse($latest_date)->lessThan(Carbon::parse(Cache::get(rayacom_config('cache_key.db_leaves_last_updated_time'))))){
             $master = session('auth_type'); $auth_data = session('auth_data');
-            if($master !== 'CUSTOMER') {
-                $query = LeaveRecord::where(['customer' => $auth_data])->where(function($Q)use($latest_date){
+            if($master === 'SALES_EXECUTIVE') {
+                $query = LeaveRecord::where(['executive' => $auth_data])->where(function($Q)use($latest_date){
+                    $Q->where('created_at','>',$latest_date)->orWhere(function($QQ)use($latest_date){
+                        $QQ->where('created_at','<=',$latest_date)->where('updated_at','>',$latest_date);
+                    });
+                });
+                if($query->count() < 50) return $query->get();
+                if($query->recent()->count() < 50) return $query->recent()->get();
+                return $query->recentAgain()->get();
+            }
+            if($master === 'ADMIN') {
+                $query = LeaveRecord::where(function($Q)use($latest_date){
                     $Q->where('created_at','>',$latest_date)->orWhere(function($QQ)use($latest_date){
                         $QQ->where('created_at','<=',$latest_date)->where('updated_at','>',$latest_date);
                     });
