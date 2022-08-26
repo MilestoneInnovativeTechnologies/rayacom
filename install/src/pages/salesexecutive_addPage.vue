8653c8632564336576ad1623653f35c190462d69
Anju KV <anju@milestoneit.net>
2022-08-26 12:53:50 +0530

    add area in SE(pending)

--- install/src/pages/salesexecutive_addPage.vue
+++ install/src/pages/salesexecutive_addPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding className="flex column q-gutter-y-sm">
-    <q-input v-model.number="ID" label="Sales Executive ID" outlined type="number"/>
+<!--    <q-input v-model.number="ID" label="Sales Executive ID" outlined />-->
     <q-input v-model="executive.name" label="Name" outlined/>
     <q-input v-model="executive.phone" label="Phone" outlined/>
     <q-input v-model="executive.email" label="Email" outlined/>
@@ -21,7 +21,8 @@ import {useRouter} from "vue-router/dist/vue-router";
 
 const masterStore = useMasterStore()
 const executives = masterStore['SALES_EXECUTIVE']
-const ID = ref(100001)
+
+// const ID = ref(100001)
 export default {
   props: ['id'],
   setup(props) {
@@ -30,44 +31,83 @@ export default {
 
 
     const ID = ref(props.id)
+    const AREA = ref(masterStore.AREA)
+    const options = []
+    // const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+    const executive = reactive({
+      id: '', name: '', password: '', email: '', phone: '', areas:[]
+    })
+    for( let n in AREA.value){
+      options.push( { label: AREA.value[n].name, value: AREA.value[n].id })
+    }
+    watchEffect(()=>{
+      // executive.value = Object.assign({}, get(executives, id), {areas: map(get(executives, [id, 'areas'], []), ({data}) => String(data))})
+      if(ID.value == 0){
+        // executive.id = executive.name = executive.password = executive.email = executive.phone = executive.areas =  ''
+        executive.value = Object.assign({}, get(executives, id), {areas: map(get(executives, [id, 'areas'], []), ({data}) => String(data))})
+      }else {
 
-    const executive = ref({}) //reactive(Object.assign({},get(executives,ID.value),{ areas:map(get(executives,[ID.value,'areas'],[]),({ data }) => String(data)) }))
-    watch(ID, (id) => {
-      executive.value = Object.assign({}, get(executives, id), {areas: map(get(executives, [id, 'areas'], []), ({data}) => String(data))})
+
+        // const executive = ref({}) //reactive(Object.assign({},get(executives,ID.value),{ areas:map(get(executives,[ID.value,'areas'],[]),({ data }) => String(data)) }))
+        // watch(ID, (id) => {
+        // executive.id = executive.name = executive.password = executive.email = executive.phone = executive.areas =  ''
+        // executive.value = Object.assign({}, get(executives, id), {areas: map(get(executives, [id, 'areas'], []), ({data}) => String(data))})
+        let aCustomer = masterStore.SALES_EXECUTIVE[ID.value]
+        console.log(aCustomer)
+        executive.id = ID.value
+        executive.name = aCustomer.name
+        executive.password = aCustomer.password
+        executive.email = aCustomer.email
+        executive.phone = aCustomer.phone
+        executive.areas = _.map(aCustomer.areas,area=>String(area.data))
+      }
+      // executive.value = Object.assign({}, get(executives, id), {areas: map(get(executives, [id, 'areas'], []), ({data}) => String(data))})
+
     })
     const areas = ref(Object.values(masterStore['AREA']))
+    // const isValidEmail = function (){
+    //   return emailPattern.test(obj.email) || 'Invalid email';
+    // }
     let msg
     let fun
     const save = function () {
       // if ((executive.name != '') && (executive.password != '') && (executive.phone != '') && (emailPattern.test(executive.email))
       // ) {
         if (ID.value > 0) {
-          post('master', 'update', {id: ID.value, ...executive.value})
+          post('master', 'update',{id: ID.value, ... executive.value})
           msg = 'updated successfully'
         } else {
-          post('add', 'store', {id: ID.value, ...executive.value})
+          post('add', 'store',{id: ID.value, ... executive.value})
           msg = 'Added a new SE successfully'
         }
         positivemsg(msg)
         router.push({
           name: 'ADMINSALESXECUTIVEPAGE'
         })
-      // }
-    }
 
-    const positivemsg = function (msg) {
-      $q.notify({
-        type: 'positive',
-        message: msg,
-        icon: 'cloud_done',
-        position: 'top-right',
-        timeout: 2000
-      })
-    }
+
+
+      }
+
+      const positivemsg = function (msg) {
+        $q.notify({
+          type: 'positive',
+          message: msg,
+          icon: 'cloud_done',
+          position: 'top-right',
+          timeout: 2000
+        })
+      }
+
 
 
     return {
       save,
+      executive,
+      areas,
+      options,
+      AREA,
+
 
       positivemsg
 
