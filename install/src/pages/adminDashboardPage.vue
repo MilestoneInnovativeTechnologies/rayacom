1de72488562edea668f52b018eb7858b3e6a3218
sona-viswam <sona.viswam@gmail.com>
2022-08-26 13:17:52 +0530

    admin dashboard changed

--- install/src/pages/adminDashboardPage.vue
+++ install/src/pages/adminDashboardPage.vue
@@ -1,10 +1,22 @@
 <template>
   <q-page padding class="flex flex-center column q-gutter-y-sm">
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="white" text-color="primary" icon-right="sick" label="LEAVES">
+        <q-badge color="positive" floating transparent>
+          4
+        </q-badge>
+      </q-btn>
+      <q-btn color="white" text-color="primary" icon-right="comment_bank" label="REVIEWS">
+        <q-badge color="accent" floating transparent>
+          4
+        </q-badge>
+      </q-btn>
+    </div>
     <div class="q-pa-md" >
       <q-toolbar class="bg-brand text-white shadow-2">
         <q-toolbar-title>ORDERS</q-toolbar-title>
       </q-toolbar>
-      <q-list bordered padding class="rounded-borders" style="max-width: 350px"
+      <q-list bordered padding class="rounded-borders" style="width:80vw; max-width: 350px"
               v-if="totalcount">
         <!--      <q-item-label header>Dashboard</q-item-label>-->
         <q-item clickable v-ripple  v-for="(i, index) in getOrders" :key="i.id"
@@ -42,100 +54,8 @@
           />
         </div>
       </q-list>
-
-
-
     </div>
-    <div class="q-pa-md" >
-      <q-toolbar class="bg-brand text-white shadow-2">
-        <q-toolbar-title>LEAVES</q-toolbar-title>
-      </q-toolbar>
-      <q-list bordered padding class="rounded-borders" style="max-width: 350px"
-              v-if="totalcount2">
-        <!--      <q-item-label header>Dashboard</q-item-label>-->
-        <q-item clickable v-ripple  v-for="(i, index) in getLeaves" :key="i.id"
-                @click="showitems(i.customer, i.id, i.date, i.status, i.items)">
-          <q-item-section avatar top>
-            <q-avatar icon="fact_check" color="brand" text-color="white" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label lines="1">{{ i.executive }}</q-item-label>
-            <q-item-label lines="1">{{ i.start_date }}</q-item-label>
-          </q-item-section>
-          <q-item-section side top>
-            <q-badge color="blue" v-if="model === 'New'" >{{ model }}</q-badge>
-            <q-badge color="secondary" v-else-if ="model === 's'" >{{ model }}</q-badge>
-            <q-badge color="positive" v-else-if="model === 'Accepted'" >{{ model }}</q-badge>
-            <q-badge color="negative" v-else-if="model === 'Rejected'" >{{ model }}</q-badge>
-            <q-badge color="primary" v-else>Unknown</q-badge>
-          </q-item-section>
-        </q-item>
-        <div class="q-pa-lg flex flex-center">
-          <q-pagination
-            v-model="page"
-            :min="currentPage"
-            :max="maxVal"
-            :max-pages="7"
-            direction-links
-            boundary-links
-            icon-first="skip_previous"
-            icon-last="skip_next"
-            icon-prev="fast_rewind"
-            icon-next="fast_forward"
-            active-color="brand"
-          />
-        </div>
-      </q-list>
 
-
-
-    </div>
-    <div class="q-pa-md" >
-      <q-toolbar class="bg-brand text-white shadow-2">
-        <q-toolbar-title>REVIEWS</q-toolbar-title>
-      </q-toolbar>
-      <q-list bordered padding class="rounded-borders" style="max-width: 350px"
-              v-if="totalcount">
-        <!--      <q-item-label header>Dashboard</q-item-label>-->
-        <q-item clickable v-ripple  v-for="(i, index) in getOrders" :key="i.id"
-                @click="showitems(i.customer, i.id, i.date, i.status, i.items)">
-          <q-item-section avatar top>
-            <q-avatar icon="fact_check" color="brand" text-color="white" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label lines="1">{{ i.customer }}</q-item-label>
-            <q-item-label lines="1">{{ i.id }}</q-item-label>
-            <q-item-label caption>{{ i.date }}</q-item-label>
-            <q-item-label caption>{{ i.narration }}</q-item-label>
-          </q-item-section>
-          <q-item-section side top>
-            <q-badge color="blue" v-if="i.status === 'New'" >{{ i.status }}</q-badge>
-            <q-badge color="secondary" v-else-if ="i.status === 'Viewed'" >{{ i.status }}</q-badge>
-            <q-badge color="accent" v-else-if="i.status === 'Accepted'" >{{ i.status }}</q-badge>
-            <q-badge color="info" v-else-if="i.status === 'Packed'" >{{ i.status }}</q-badge>
-            <q-badge color="blue-grey" v-else-if="i.status === 'Dispatched'" >{{ i.status }}</q-badge>
-            <q-badge color="primary" v-else>Unknown</q-badge>
-          </q-item-section>
-        </q-item>
-      </q-list>
-
-      <div class="q-pa-lg flex flex-center" v-if="totalcount">
-        <q-pagination
-          v-model="page"
-          :min="currentPage"
-          :max="maxVal"
-          :max-pages="7"
-          direction-links
-          boundary-links
-          icon-first="skip_previous"
-          icon-last="skip_next"
-          icon-prev="fast_rewind"
-          icon-next="fast_forward"
-          active-color="brand"
-        />
-      </div>
-
-    </div>
 
     <div class="q-pa-md q-gutter-sm">
       <q-dialog v-model="card">
@@ -268,37 +188,8 @@ export default {
       return Object.values(leaveStore.leaves)
     })
 
-    const MYLEAVES =  computed(() => {
-      let newArray = []
-      for( let n in LEAVES.value){
-        checkstatus = LEAVES.value[n]['status']
-        if(checkstatus == 'New'){
-          newArray.push(LEAVES.value[n])
-        }
-      }
-      return newArray.reverse();
-    })
-    console.log(MYLEAVES.value)
-    const totalcount2 =  computed(() => {
-      return Object.keys(MYLEAVES.value).length
-    })
 
-    const REVIEWS =  computed(() => {
-      return Object.values(reviewStore.reviews)
-    })
-    const MYREVIEWS =  computed(() => {
-      let newArray = []
-      for( let n in REVIEWS.value){
-        checkstatus = REVIEWS.value[n]['status']
-        if(checkstatus == 'New'){
-          newArray.push(REVIEWS.value[n])
-        }
-      }
-      return newArray.reverse();
-    })
-    const totalcount3 =  computed(() => {
-      return Object.keys(MYREVIEWS.value).length
-    })
+
 
     let MYKEYS
     let newArr
@@ -385,19 +276,18 @@ export default {
     let page = ref(1)
     let currentPage= ref(1)
     const totalPages= ref(5)
-
     const maxVal =  computed(() => {
       return Math.ceil(totalcount.value/totalPages.value)
     })
 
     return {
-      MYORDERS, MYLEAVES, MYREVIEWS,
-      totalcount, totalcount2, totalcount3,
+      MYORDERS,
+      totalcount,
       page,
       currentPage,
       totalPages,
       maxVal,
-      getOrders, getLeaves, getReviews,
+      getOrders,
       showitems,
       specificItems,
       specificId,
