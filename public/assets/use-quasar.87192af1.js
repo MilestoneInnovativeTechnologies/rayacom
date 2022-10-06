d13d11185124da51a4f66d72cca23c5cf0b8f2b4
Firose Hussain <github@firumon.com>
2022-10-06 15:24:07 +0530

    New build 0.1.9

--- public/assets/use-quasar.87192af1.js
+++
@@ -1,1 +0,0 @@
-import{F as a,ax as r}from"./index.d755ac14.js";function u(){return a(r)}export{u};
---
+++ public/assets/use-quasar.c67c6e3f.js
@@ -0,0 +1,1 @@
+import{F as a,ax as r}from"./index.8ca15aea.js";function u(){return a(r)}export{u};
