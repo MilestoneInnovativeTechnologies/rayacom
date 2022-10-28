3a0cd0c3a8aa3ed1965070f34d9f513190d7912e
sona-viswam <sona.viswam@gmail.com>
2022-10-28 10:33:51 +0530

    field added in offer table

--- src/Controllers/OfferController.php
+++ src/Controllers/OfferController.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Cache;
 class OfferController extends Controller
 {
   public function store(){
-    $data = new Review(request()->only(['item', 'minimum_quantity', 'offer_quantity', 'type', 'customers', 'status']));
+    $data = new Review(request()->only(['item', 'minimum_quantity', 'offer_quantity', 'type', 'customers', 'Approved']));
     $data->save();
     return $data;
   }
