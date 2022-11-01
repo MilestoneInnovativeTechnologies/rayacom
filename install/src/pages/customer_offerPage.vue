40aaecad786706a5a9ab9b92b9b39a6464b894ff
Anju KV <anju@milestoneit.net>
2022-11-01 17:03:54 +0530

    customer offer pending

--- install/src/pages/customer_offerPage.vue
+++ install/src/pages/customer_offerPage.vue
@@ -1,96 +1,69 @@
 <template>
-  <q-page padding class="flex column q-col-gutter-y-lg">
 
-    <div class="q-pa-sm">
-      <div class="row q-pa-md">
-        <div class="col-8">
-          <q-input
-            v-model="search"
-            debounce="500"
-            filled
-            placeholder="Search">
-            <template v-slot:append>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-        </div>
-<!--        <div class="q-pa-md row items-start q-gutter-md" v-if="myarrayLength">-->
-<!--          <q-card flat bordered-->
-<!--                  class="my-card"-->
-<!--                  v-for="(i, index) in getData" :key="i.id"-->
-<!--                  @click="selectedProducts(i.id, i.name)">-->
-<!--            <q-card-section class="bg-brand text-white">-->
-<!--              <div class="Subtitle 2 text-weight-bolder">{{ i.name }}</div>-->
-<!--            </q-card-section>-->
-<!--          </q-card>-->
-<!--        </div>-->
-<!--        <div class="col-4 text-center">-->
-<!--          <q-btn color="positive" no-caps @click="gotoAction(0)">-->
-<!--            <q-icon left size="xl" name="open_in_new" />-->
-<!--            ADD OFFER-->
-<!--          </q-btn>-->
-<!--        </div>-->
-      </div>
+  <div class="q-pa-md fullwidth">
+    <div class="q-gutter-md">
+      <q-input
+        v-model="search"
+        debounce="500"
+        filled
+        placeholder="Search"
+      >        <template v-slot:append><q-icon name="search" /></template>
+      </q-input>
     </div>
-    <div class="q-pa-sm row items-start q-gutter-md" v-if="myarrayLength">
-      <q-card flat bordered
-              class="my-card"
-              v-for="(i, index) in getData" :key="i.id"
-              @click="selectedProducts(i.id, i.name)">
-        <q-card-section class="bg-brand text-white">
-          <div class="Subtitle 2 text-weight-bolder">{{ i.name }}</div>
-        </q-card-section>
-        <q-list>
-          <q-item class="bg-brand text-white text-bold">
-            <q-item-section>
-              <q-item-label>{{ i.item.name }}</q-item-label>
-              <q-item-label class="text-subtitle2">Qty: {{ i.minimum_quantity }}</q-item-label>
-              <q-item-label>
-                <q-badge color="blue" v-if="i.status === 'New'" >{{ i.status }}</q-badge>
-                <q-badge color="secondary" v-else-if ="i.status === 'Approved'" >{{ i.status }}</q-badge>
-                <q-badge color="positive" v-else-if="i.status === 'Published'" >{{ i.status }}</q-badge>
-                <q-badge color="accent" v-else-if="i.status === 'Inactive'" >{{ i.status }}</q-badge>
-                <q-badge color="dark" v-else>Unknown</q-badge>
-              </q-item-label>
-            </q-item-section>
+  </div>
 
-<!--            <q-item-section avatar>-->
-<!--              <q-btn flat round color="primary" icon="mode_edit" @click="gotoAction(i.id)">-->
-<!--                <q-tooltip> Edit </q-tooltip>   </q-btn>-->
-<!--            </q-item-section>-->
-          </q-item>
-        </q-list>
-      </q-card>
-    </div>
-    <div class="q-pa-lg flex flex-center" v-if="myarrayLength">
-      <q-pagination
-        v-model="page"
-        :min="currentPage"
-        :max="maxVal"
-        :max-pages="7"
-        direction-links
-        boundary-links
-        icon-first="skip_previous"
-        icon-last="skip_next"
-        icon-prev="fast_rewind"
-        icon-next="fast_forward"
-        active-color="deep-orange-10"
-      />
-    </div>
-    <div class="q-pa-md q-gutter-sm" v-if="myproducts.length">
-      <q-btn unelevated rounded color="positive" label="Proceed"  icon-right="send" class="full-width"
-             @click="order()">
-      </q-btn>
-    </div>
-  </q-page>
+  <div class="q-pa-md row items-start q-gutter-md" v-if="myitemsLength">
+    <q-card flat bordered
+            class="my-card"
+            v-for="(i, index) in getData" :key="i.id"
+            @click="selectedProducts(i.id, i.item.name)">
+
+      <q-card-section class="bg-brand text-white">
+        <q-item-label>{{ i.item.name }}</q-item-label>
+        <q-item-label>quantity :{{ i.minimum_quantity }}</q-item-label>
+        <q-item-label>offer  :{{ i.offer_quantity }}</q-item-label>
+<!--        <div class="Subtitle 2 text-weight-bolder">{{ i.name }}</div>-->
+<!--        <div class="Subtitle 2 text-weight-bolder">{{ i.quantity }}</div>-->
+<!--        <div class="Subtitle 2 text-weight-bolder">{{ i.offer_quantity }}</div>-->
+      </q-card-section>
+    </q-card>
+  </div>
+
+<!--    <div class="q-mt-sm">-->
+<!--      {{myproducts}}-->
+<!--    </div>-->
+
+  <div class="q-pa-lg flex flex-center" v-if="myitemsLength">
+    <q-pagination
+      v-model="page"
+      :min="currentPage"
+      :max="Math.ceil(myitemsLength/totalPages)"
+      :max-pages="7"
+      direction-links
+      boundary-links
+      icon-first="skip_previous"
+      icon-last="skip_next"
+      icon-prev="fast_rewind"
+      icon-next="fast_forward"
+      active-color="brand"
+    />
+  </div>
+
+  <div class="q-pa-md q-gutter-sm" v-if="myproducts.length">
+    <q-btn unelevated rounded color="positive" label="Proceed"  icon-right="send" class="full-width"
+           @click="order()">
+    </q-btn>
+  </div>
+
 </template>
+
 <script>
 import { computed, ref  } from 'vue'
 import { useOfferStore } from 'stores/offers'
+import { useQuasar } from 'quasar'
 import { useRouter } from 'vue-router'
 const offerStore = useOfferStore()
 import { useMasterStore } from 'stores/master'
-import {useQuasar} from "quasar";
 const master = useMasterStore()
 export default {
   setup () {
@@ -102,9 +75,10 @@ export default {
 
     const search = ref('')
 
-    const MYOFFER = computed(()=>{
-      return master.OFFERS
-    })
+    // const MYOFFER = computed(()=>{
+    //   return master.OFFERS
+    // })
+
     const searchResult = computed(()=>{
       if(search.value === ''){
         return Object.values(offerStore.offers)
@@ -113,9 +87,11 @@ export default {
         return Object.values(offerStore.offers).filter(word => word.item.name.toLowerCase().indexOf(keyword) > -1);
       }
     })
-    const myarrayLength = computed(()=>{
+
+    const myitemsLength = computed(()=>{
       return searchResult.value.length
     })
+
     const getData =  computed(() => {
       num1 = (page.value-1)*totalPages.value;
       num2 = (page.value-1)*totalPages.value+totalPages.value;
@@ -125,6 +101,7 @@ export default {
       })
       return newArr
     })
+
     const selectedProducts = function(item, itemname ){
       var msg =''
       var exists = myproducts.value.some(function(field) {
@@ -139,6 +116,7 @@ export default {
       }
       positivemsg(msg)
     }
+
     const positivemsg = function (msg){
       $q.notify({
         type: 'positive',
@@ -148,6 +126,7 @@ export default {
         timeout:800
       })
     }
+
     const order = function (){
       var products = JSON.stringify(myproducts.value)
       router.push({
@@ -156,36 +135,26 @@ export default {
       })
     }
 
-
-
-    // const gotoAction = function (id){
-    //   router.push({
-    //     name: 'OFFERADD', state: { id },
-    //   })
-    // }
-
     let page = ref(1)
     let currentPage= ref(1)
+    let nextPage= ref(null)
     const totalPages= ref(10)
 
-    const maxVal =  computed(() => {
-      return Math.ceil(myarrayLength.value/totalPages.value)
-    })
     return {
-      MYOFFER,
+
       search,
       searchResult,
-      myarrayLength,
+      // searchOffer,
+      myitemsLength,
       page,
       currentPage,
+      nextPage,
       totalPages,
-      maxVal,
       getData,
-      order,
+      positivemsg,
       myproducts,
       selectedProducts,
-      positivemsg,
-      gotoAction
+      order,
     }
   },
 }
